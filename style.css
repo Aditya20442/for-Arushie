d432ae2ec54422f4b57bc0dfa60ae9c400555ea2
YASH7CHAUDHARI <yashchau1409@gmail.com>
2026-07-31 12:42:46 +0530

    Add CSS styles for layout and card component

--- style.css
+++ style.css
@@ -1,1 +1,114 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
 
+body{
+    font-family:'Poppins',sans-serif;
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    overflow:hidden;
+
+    background:linear-gradient(135deg,#ffd9ec,#ffeef7,#fff8fc);
+}
+
+.card{
+
+    width:90%;
+    max-width:420px;
+
+    padding:40px;
+
+    text-align:center;
+
+    border-radius:30px;
+
+    background:rgba(255,255,255,.45);
+
+    backdrop-filter:blur(18px);
+
+    box-shadow:
+    0 15px 45px rgba(255,105,180,.25);
+
+    animation:fadeUp 1s ease;
+}
+
+.flower{
+
+    font-size:55px;
+
+    margin-bottom:10px;
+
+}
+
+h1{
+
+    font-family:'Pacifico',cursive;
+
+    color:#ff4f8b;
+
+    font-size:42px;
+
+    margin-bottom:12px;
+
+}
+
+p{
+
+    color:#555;
+
+    font-size:18px;
+
+    margin-bottom:35px;
+
+}
+
+button{
+
+    border:none;
+
+    padding:16px 34px;
+
+    border-radius:999px;
+
+    background:#ff4f8b;
+
+    color:white;
+
+    font-size:17px;
+
+    font-weight:600;
+
+    cursor:pointer;
+
+    transition:.3s;
+}
+
+button:hover{
+
+    transform:scale(1.08);
+
+}
+
+@keyframes fadeUp{
+
+from{
+
+opacity:0;
+
+transform:translateY(50px);
+
+}
+
+to{
+
+opacity:1;
+
+transform:translateY(0);
+
+}
+
+}
